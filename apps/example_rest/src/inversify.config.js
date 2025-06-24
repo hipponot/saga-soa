ff5361ccaa4a6e1d74490abd0991a86cec3a31b4
Sean Kelly <skelly@saga.org>
2025-06-24 16:07:25 -0600

    example_rest uses tsup to generate node resolvable JS

--- apps/example_rest/src/inversify.config.js
+++
@@ -1,9 +0,0 @@
-import { Container } from 'inversify';
-import { HelloRest } from './sectors/hello-rest';
-export const TYPES = {
-    RestEndpointGroup: Symbol.for('RestEndpointGroup'),
-};
-const container = new Container();
-// Multi-binding for sectors
-container.bind(TYPES.RestEndpointGroup).to(HelloRest);
-export { container };
--- apps/example_rest/src/sectors/hello-rest.d.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { RestEndpointGroup } from '@saga-soa/core-api/rest/rest-endpoint-group';
-export declare class HelloRest extends RestEndpointGroup {
-    constructor();
-    protected registerSectorRoutes(router: Router, sectorBase: string): void;
-    private helloRoute;
-}
-//# sourceMappingURL=hello-rest.d.ts.map
