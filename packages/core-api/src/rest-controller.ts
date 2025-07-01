8ac403a7e468a360702c216b63224dd5fa401826
Sean Kelly <skelly@saga.org>
2025-07-01 13:20:51 -0600

    crud from cursor

--- packages/core-api/src/rest-controller.ts
+++ packages/core-api/src/rest-controller.ts
@@ -1,7 +1,7 @@
-import { Container }                             from 'inversify';           from 'inversify';    from 'routing-controllers';
-import { injectable, inject }             from 'inversify';
-import { HelloRest }                             from './sectors/hello-rest';from './sectors/hello-rest';saga-soa/logger';
-import figlet                             from 'figlet';
+import { Get, Controller }    from 'routing-controllers';
+import { injectable, inject } from 'inversify';
+import type { ILogger }       from '@saga-soa/logger';
+import figlet                 from 'figlet';
 
 export const REST_API_BASE_PATH = 'saga-soa';
 
