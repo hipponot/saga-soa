d3ab6225876ab7125424cf6f6ae4a8669b8cc3ed
Sean Kelly <skelly@saga.org>
2025-07-01 14:52:18 -0600

    remove relic reg code

--- apps/examples/rest-api/src/sectors/hello-again-rest.ts
+++ apps/examples/rest-api/src/sectors/hello-again-rest.ts
@@ -1,11 +1,11 @@
-import { Get }                                                    from 'routing-controllers';
-import { inject }                                                 from 'inversify';
-import type { ILogger }                                           from '@saga-soa/logger';
-import { RestControllerBase, RestController, REST_API_BASE_PATH } from '@saga-soa/core-api/rest-controller';
+import { Get, Controller }                                                    from 'routing-controllers';
+import { inject }                                                             from 'inversify';
+import type { ILogger }                                                       from '@saga-soa/logger';
+import { RestControllerBase, REST_API_BASE_PATH }                             from '@saga-soa/core-api/rest-controller';
 
 const SECTOR = 'hello-again';
 
-@RestController(`/${REST_API_BASE_PATH}/${SECTOR}`)
+@Controller(`/${REST_API_BASE_PATH}/${SECTOR}`)
 export class HelloAgainRest extends RestControllerBase {
   readonly sectorName = SECTOR;
   constructor(@inject('ILogger') logger: ILogger) { super(logger, SECTOR); }
--- packages/core-api/src/rest-controller.ts
+++ packages/core-api/src/rest-controller.ts
@@ -5,29 +5,10 @@ import figlet                 from 'figlet';
 
 export const REST_API_BASE_PATH = 'saga-soa';
 
-export function RestController(path: string) {
-  const controllerDecorator = Controller(path);
-  return function (target: any) {
-    // Apply the routing-controllers @Controller decorator
-    controllerDecorator(target);
-    // Auto-register for your own tracking
-    if (typeof target.registerController === 'function') {
-      target.registerController(target);
-    }
-  };
-}
-
 export abstract class RestControllerBase {
+
   private static _controllers: Function[] = [];
   protected logger: ILogger;
-
-  static registerController(controller: Function) {
-    RestControllerBase._controllers.push(controller);
-  }
-
-  static getRegisteredControllers() {
-    return RestControllerBase._controllers;
-  }
 
   abstract readonly sectorName: string;
 
@@ -37,7 +18,7 @@ export abstract class RestControllerBase {
 
   @Get('/')
   home() {
-    const splash = figlet.textSync(this.sectorName, { font: 'Alligator' });
+    const splash = figlet.textSync(this.sectorName, { font: 'Standard' });
     return `<pre>${splash}</pre>`;
   }
 
