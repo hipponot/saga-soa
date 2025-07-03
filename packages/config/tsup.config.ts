a49353ba523eb49e2fd196107ed907ddba337b69
Sean Kelly <skelly@saga.org>
2025-07-03 16:08:55 -0600

    tsup across the board

---
+++ packages/config/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+    'src/config-manager.ts',
+    'src/config-validation-error.ts',
+    'src/dotenv-config-manager.ts',
+    'src/i-config-manager.ts',
+  ],
+  format: ['esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  outDir: 'dist',
+  splitting: false,
+  skipNodeModulesBundle: true,
+}); 
