61cc3f14256addeebb7abb3a247e5c1d6618427f
Seth Paul <sethcpaul@gmail.com>
2025-07-21 12:15:55 -0600

    move out examples

--- apps/examples/rest-api/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, type Options } from "tsup";
-
-export default defineConfig((options: Options) => ({
-  entry: ["src/**/*"],
-  clean: true,
-  format: ["esm"],
-  sourcemap: true,
-  ...options,
-})); 
